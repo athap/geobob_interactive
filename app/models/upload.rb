ab7d0ee66ed29d244ad11bf6587cca9f1557e39f
Justin Ball <justinball@gmail.com>
2010-12-15 22:02:10 -0700

    fixed upload

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -26,24 +26,9 @@
 
 class Upload < ActiveRecord::Base
 
-  acts_as_uploader  :enable_s3 => false,
-                    :has_attached_file => {
-                      :url     => "/system/:attachment/:id_partition/:style/:basename.:extension",
-                      :path    => ":rails_root/public/system/:attachment/:id_partition/:style/:basename.:extension",
-                      :styles  => { :icon => "30x30!", 
-                                    :thumb => "100>", 
-                                    :small => "150>", 
-                                    :medium => "300>", 
-                                    :large => "660>" },
-                      :default_url => "/images/profile_default.jpg",
-                      :storage => :s3,
-                      :s3_credentials => AMAZON_S3_CREDENTIALS,
-                      :bucket => "assets.#{GlobalConfig.application_url}",
-                      :s3_host_alias => "assets.#{GlobalConfig.application_url}",
-                      :convert_options => {
-                         :all => '-quality 80'
-                       }
-                    },
-                    :s3_path => ':id_partition/:style/:basename.:extension'
-
+  include Uploader::Models::Upload
+  
+  validates_attachment_presence :local
+  validates_attachment_size :local, :less_than => 10.megabytes
+  
 end
