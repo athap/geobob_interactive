aa022f6d1791a93b96ed239cb6d1d53fb4469d29
Justin Ball <justinball@gmail.com>
2011-05-13 13:47:28 -0600

    changed secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Geobob::Application.config.secret_token = '7c0ce846b7a1c60f4ee7b7e5e6e1197f4106b8bcce5ff5eed8d13a2af4704e96a956da44ae8fe2d4d78546fffe45ede80bd025fcbb53515b78f0835655ecdd70'
+Geobob::Application.config.secret_token = '8c0ce846b7a1c60f4ee7b7e5e6e1197f4106b8bcce5ff5eed8d13a2af4704e96a956da44ae8fe2d4d78546fffe45ede80bd025fcbb53515b78f0835655ecdd70'
